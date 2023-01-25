98cd1aa39894db5dd4cd9a9aed4597a4077d0bee
Bandi <jagadeesh.bandi@capgemini.com>
2023-01-25 23:17:45 +0530

    Intial Commit

---
+++ src/main/java/com/example/counterdocker/CounterDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.counterdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/counterdocker/CounterDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.counterdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
